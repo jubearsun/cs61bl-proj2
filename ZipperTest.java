a0be374892dce87f70bd8d36db969c7ba70819cf
bsmarimon <bsmarimon@berkeley.edu>
2014-08-02 17:47:17 -0700

    Update ZipperTest.java

--- ZipperTest.java
+++ ZipperTest.java
@@ -1,7 +1,5 @@
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
+import java.util.ArrayList;
 
 import junit.framework.TestCase;
 
